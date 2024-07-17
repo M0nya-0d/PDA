8c1fa348cfd8b154fc0dd710f0630234fb84706a
M0nya-0d <159374625+M0nya-0d@users.noreply.github.com>
2024-07-17 13:14:54 +0300

    Update job.py

--- job.py
+++ job.py
@@ -1,5 +1,12 @@
 #Job
 import os
-print('ky-ky')
+user_name = 'kyky'
+print(user_name)
 while True:
     os.system('python3 test.py')
+    if (user_name == 'kyky'):
+        user_name = 'dada'
+        print(user_name)
+        
+    
+    
