9d9ecccd70c9e1d86c9996ef4afbec70d48a888d
M0nya-0d <159374625+M0nya-0d@users.noreply.github.com>
2024-07-17 12:30:33 +0300

    Update job.py

--- job.py
+++ job.py
@@ -3,9 +3,10 @@ inc = None
 file = open('inc.txt', 'r', encoding='utf-8')
 inc = int(file.read())
 file.close()
-
-if(inc == 1):
-    #inc = 50
-    file = open('inc.txt', 'w', encoding='utf-8')
-    file.write('55')
-    file.close()
+print(inc)
+while True:
+    if(inc == 1):
+        file = open('inc.txt', 'w', encoding='utf-8')
+        file.write('55')
+        file.close()
+        print(inc)
