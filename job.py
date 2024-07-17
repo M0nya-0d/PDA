4a99ee118dc12774687e3102293cf6891f731566
M0nya-0d <odessatehnolink@gmail.com>
2024-07-17 12:55:11 +0300

    test2

--- job.py
+++ job.py
@@ -1,11 +1,4 @@
 #Job
-inc = None
-file = open('inc.txt', 'r', encoding='utf-8')
-inc = int(file.read())
-file.close()
-
-if(inc == 1):
-    #inc = 50
-    file = open('inc.txt', 'w', encoding='utf-8')
-    file.write('55')
-    file.close()
+import os
+while True:
+    os.system('python3 test.py')
---
+++ test.py
@@ -0,0 +1,6 @@
+import time
+name_user = 'faqrrsdf'
+def bbbb():
+    print(name_user)
+time.sleep(10)
+bbbb()
