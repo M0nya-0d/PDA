b69743e5d92c7f2e29732f1524d0a974591ed5f6
M0nya-0d <159374625+M0nya-0d@users.noreply.github.com>
2024-07-17 12:44:36 +0300

    Update job.py

--- job.py
+++ job.py
@@ -1,12 +1,4 @@
 #Job
-inc = None
-file = open('inc.txt', 'r', encoding='utf-8')
-inc = int(file.read())
-file.close()
-print(inc)
+import os
 while True:
-    if(inc == '1'):
-        file = open('inc.txt', 'w', encoding='utf-8')
-        file.write('55')
-        file.close()
-        print(inc)
+    ios.system('python3 start.py')
