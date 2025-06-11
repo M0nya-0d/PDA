ed4f451da03ee08a8f3e98cdb5f31e52849c0e11
M0nya-0d <odessatehnolink@gmail.com>
2025-06-11 17:46:50 +0300

    new file:   stall/param.json 	modified:   stall/stall.py 	modified:   vers.txt

--- stall/stall.py
+++ stall/stall.py
@@ -1,5 +1,6 @@
 import serial
 import time
+import json
 
 serial_port = "/dev/ttyS5"
 baud_rate = 115200
@@ -12,27 +13,47 @@ def int_write(addr, num):
         (addr >> 8) & 0xFF, addr & 0xFF,
         (num >> 8) & 0xFF, num & 0xFF
     ])
-    print(f'Send to {hex(addr)}:', packet.hex())
     with serial.Serial(serial_port, baud_rate, timeout=1) as ser:
         ser.write(packet)
-        response = ser.read(32)
-        print('Ответ:', response.hex())
 
-HP = 0
-RD = 1000
-
-while True:
-    # Увеличиваем HP
-    if HP < 10000:
-        HP += 1
-
-    # Уменьшаем RD
+def update_hp_rd(HP, RD):
     if 0 < RD < 4000:
         RD -= 1
+        if HP < 10000:
+            HP += 1
+    elif 4000 < RD < 7000:
+        RD -= 1
+    elif 7000 < RD < 8000:
+        HP -= 10
+        if HP < 0:
+            HP = 0
+    elif 8000 < RD < 9000:
+        HP -= 20
+        if HP < 0:
+            HP = 0
+    return HP, RD
 
-    # Отправляем значения
-    int_write(0x5000, HP)   # VP = 0x5000
-    int_write(0x5001, RD)   # VP = 0x5001 (создай такую переменную в редакторе!)
+def load_params(filename):
+    with open(filename, "r") as f:
+        data = json.load(f)
+    return data["HP"], data["RD"]
 
-    print(f'HP = {HP}, RD = {RD}')
-    time.sleep(1)
+def main():
+    # Загружаем стартовые параметры из файла
+    HP, RD = load_params("param.json")
+    print(f"Стартовые значения: HP = {HP}, RD = {RD}")
+
+    last_update = time.monotonic()
+    while True:
+        now = time.monotonic()
+        if now - last_update >= 1.0:
+            last_update = now
+            HP, RD = update_hp_rd(HP, RD)
+            int_write(0x5000, HP)
+            int_write(0x5001, RD)
+            print(f'HP = {HP}, RD = {RD}')
+        # Здесь могут быть другие задачи
+        time.sleep(0.01)
+
+if __name__ == "__main__":
+    main()
