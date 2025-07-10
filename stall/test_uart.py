cdfac0a6cab2e42fdc4fe7e31b844aef95afbc2a
M0nya-0d <odessatehnolink@gmail.com>
2025-07-10 15:59:21 +0300

    modified:   stall/test_uart.py 	modified:   vers.txt

--- stall/test_uart.py
+++ stall/test_uart.py
@@ -1,20 +1,8 @@
 import serial
+import time
 
-try:
-    ser = serial.Serial("/dev/ttyS1", 9600, timeout=1)
-    print("✅ JDY-40 слушает на /dev/ttyS1...")
+ser = serial.Serial("/dev/ttyS2", 9600)
 
-    while True:
-        if ser.in_waiting > 0:
-            data = ser.readline().decode(errors='ignore').strip()
-            if data:
-                print(f"📡 Получено от JDY-40: {data}")
-
-except KeyboardInterrupt:
-    print("\n🔌 Завершено вручную.")
-except Exception as e:
-    print("❌ Ошибка:", e)
-finally:
-    if 'ser' in locals() and ser.is_open:
-        ser.close()
-        print("🔒 Соединение закрыто.")
+while True:
+    ser.write(b"Hello from TX2\n")
+    time.sleep(1)
