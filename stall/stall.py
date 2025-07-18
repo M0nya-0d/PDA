af07248802a99f2f73c75f8df3196070b6ef2c87
M0nya-0d <odessatehnolink@gmail.com>
2025-07-18 17:30:05 +0300

    modified:   stall/stall.py 	modified:   vers.txt

--- stall/stall.py
+++ stall/stall.py
@@ -200,7 +200,6 @@ def update_hp_rd(HP, RD):
     if flag_radic and norma:
         send_packets.append(bytes([0x5A, 0xA5, 0x07, 0x82, 0x00, 0x84, 0x5A, 0x01, 0x00, 0x16]))
         radic_up += 1
-        send_packets.append(bytes([0x5A, 0xA5, 0x07, 0x82, 0x00, 0xA0, 0x01, 0x01, 0x40, 0x00]))
         if radic_up >= 5:
             send_packets.append(bytes([0x5A, 0xA5, 0x07, 0x82, 0x00, 0x84, 0x5A, 0x01, 0x00, 0x00]))
             flag_radic = False
@@ -296,7 +295,7 @@ def update_hp_rd(HP, RD):
     print(f"[DEBUG] update_hp_rd: orig_HP={orig_HP}, orig_RD={orig_RD}, new_HP={HP}, new_RD={RD}, changed={changed}")
     return HP, RD, changed, send_packets
 
-def radic():
+def radic(send_packets):
     global RD, HP, flag_radic, arm_rad, block_rad, rad_stat, RD_stat
     if not block_rad:
         flag_radic = True
@@ -312,6 +311,7 @@ def radic():
         else:
             RD += 100
             HP -= 50
+        send_packets.append(bytes([0x5A, 0xA5, 0x07, 0x82, 0x00, 0xA0, 0x01, 0x01, 0x40, 0x00]))    
 
 def art_type(device_type):
     global last_device_type
